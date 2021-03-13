cb4c28f50a66c42a27a75aec79db1e02ea760bc6
kian <king.kian007@gmail.com>
2021-03-13 23:41:21 +0330

    t

--- beautyandhealth.php
+++ beautyandhealth.php
@@ -77,6 +77,12 @@ if($lamp ){
 			setTimeout(showSlides, 2000); // Change image every 2 seconds
 			}
 		</script> 
+		<style>
+			form
+			{
+				background-color: rgba(204, 0, 102, 0.7)
+			}
+		</style>
 	</head>
 	<body onload="showSlides()">
 	<a name="start"></a>
