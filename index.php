5ee2fc6ca7a44274b58c4bfbcf093eaa36a7e6b9
kian <king.kian007@gmail.com>
2021-03-14 20:19:56 +0330

    footer

--- index.php
+++ index.php
@@ -181,7 +181,7 @@
 				</div>
 			</div>
 			<div class="footer">
-				 <p style="float: bottom"><span style="color:red">  2021©  کلیه حقوق این سایت محفوظ میباشد</span></p>
+				 <p style="position: absolute; top: 500px"><span style="color:red">  2021©  کلیه حقوق این سایت محفوظ میباشد</span></p>
 				 <b><a  href="contactus.php"	 style="text-decoration: none; float: right;position:relative;top: -25px; right: 50px; color: #222">تماس با ما</a></b>
 			</div>
 		</div>
