f4e61ce0c54cf71226ecdfcdeea50ef367e6b446
kian <king.kian007@gmail.com>
2021-03-14 19:12:33 +0330

    t

--- index.php
+++ index.php
@@ -53,10 +53,10 @@
 	<body onload="showSlides()">
 	<a name="start"></a>
 		<div class="container"> 
-				<img class = "logo" src = 'images\kiki.jpg'  />
-			<div class="header"> 
 				<h1 class ="h1">KikiCutest</h1>
+				<img class = "logo" src = 'images\kiki.jpg'  />
 				<p class ="logoText">KikiCutest</p>
+			<div class="header"> 
 				<div class= "menue">
 				<ul class ="ulmenue" >
 					<li><a href='#'>آرایشی و بهداشتی</a>
