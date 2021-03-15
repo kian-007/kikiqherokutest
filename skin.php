d052de415859b60e13f723550c859e2c5091d7e7
kian <king.kian007@gmail.com>
2021-03-15 18:23:33 +0330

    add images to skin for facebrush

--- skin.php
+++ skin.php
@@ -1,3 +1,26 @@
+<?php 
+include_once('beautyandhealth.php')
+
+$facebrush = get_value('facebrush' , 0)
+$handcream = get_value('handcream' , 0)
+
+if($handcream ){
+    if($facebrush){
+        $title = 'فیس براش چکشی';
+    }else{
+        $title = 'کرم مرطوب کننده دست';
+    }
+}else{
+        if($facebrush){
+           $title = 'فیس براش چکشی';
+        }else{
+           $title = 'پوست';
+        }
+    }
+?>
+
+
+
 <html>
 	<head>
 		<meta charset="UTF-8" />
@@ -108,16 +131,42 @@
 				<div class="left2">
 				</div>
 				<div class="content2">
-					<div class="doutside ftl d1"><img	id='img1'	src="images/skin/ax1.jpg"	/><img	id='img2' src="images/skin/mrtb99.jpg"	/><p id='p1'><p class="pinside"></p></div>
-					<div class="doutside ftl"><img	src="images/skin/mrtb44.jpg"	/><p class="pinside"></p></div>
-					<div class="doutside ftl"><img	src="images/skin/mrtb55.jpg"	/><p class="pinside"></p></div>
-					<div class="doutside ftl"><img	src="images/skin/mrtb66.jpg"	/><p class="pinside"></p></div>
-					<div class="doutside ftl"><img	src="images/skin/mrtb77.jpg"	/><p class="pinside"></p></div>
-					<div class="doutside ftl"><img	src="images/skin/mrtb88.jpg"	/><p class="pinside"></p></div>
+				
+				<?php if($handcream): ?>
+					<div class="doutside ftr d1"><img	id='img1'	src="images/skin/ax1.jpg"	/><img	id='img2' src="images/skin/mrtb99.jpg"	/><p id='p1'><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/mrtb44.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/mrtb55.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/mrtb66.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/mrtb77.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/mrtb88.jpg"	/><p class="pinside"></p></div>
 					<div class="dani"><img	style="position:absolute; top:4px; float:right; border-radius:45px" src="images/up.jpg"	/><a id='contact' href="#start" style="position:absolute; text-decoration: none; "><p class="pani"></p></a></div>
+					<?php endif; ?>
+					
+				<?php if($facebrush): ?>
+					<div class="doutside ftr d1"><img	id='img1'	src="images/skin/facebrush1.jpg"	/><img	id='img2' src="images/skin/brush4.jpg"	/><p id='p1'><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/brush2.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/brush7.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/brush6.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/brush5.jpg"	/><p class="pinside"></p></div>
+					<div class="doutside ftr"><img	src="images/skin/brush3.jpg"	/><p class="pinside"></p></div>
+					<div class="dani"><img	style="position:absolute; top:4px; float:right; border-radius:45px" src="images/up.jpg"	/><a id='contact' href="#start" style="position:absolute; text-decoration: none; "><p class="pani"></p></a></div>
+					<?php endif; ?>
+					
 				</div>
 				
 				<div class="right2">
+				<?php if($lamp || $capsul): ?>
+					<legend><span style="font-size:15pt"><b>Search what you are intrested in:</b></span></legend>
+					<form action="beautyandhealth.php" >
+						<input type="checkbox"    name="handcream"    id="handcream"    value="show" />
+						<label for="handcream"><b>کرم مرطوب کننده دست</b></label><br/>
+						
+						<input type="checkbox"    name="facebrush"    id="facebrush"    value='show' />
+						<label for="facebrush"><b>فیس براش چکشی هولوگرامی</b></label><br/>
+						
+						<input type="submit" value="جست و جو" />
+					</form>
+					<?php endif; ?>
 				</div>
 			</div>
 			<div class="footer">
