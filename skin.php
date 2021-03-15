aae5c925a7d7e128b748e8692e2ae4091e59c9f4
kian <king.kian007@gmail.com>
2021-03-15 18:37:40 +0330

    fixed php codes

--- skin.php
+++ skin.php
@@ -1,5 +1,12 @@
 <?php 
-include_once('beautyandhealth.php');
+function get_value($para, $default){
+    if(isset($_GET[$para])){
+        $value = $_GET[$para];
+    }else{
+        $value = $default;
+    }
+    return $value;
+}
 
 $facebrush = get_value('facebrush' , 0);
 $handcream = get_value('handcream' , 0);
