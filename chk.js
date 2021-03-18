b42c381751fe0187378d75375175af8dcd478b57
kian <king.kian007@gmail.com>
2021-03-18 17:40:33 +0330

    add plugin to checkboxes

--- chk.js
+++ chk.js
@@ -4,16 +4,15 @@
 			$(this).click(function(){
 				if(this.checked)
 				{
-					$(this).addClass('mychk')
+					$(this).toggleClass('mychk')
 					$(this).hide()
-					if($(this).not('mychk'))
+					if($(this).hasClass('mychk'))
 					{
 					$(this).after("<img src='images/chk2.jpg' />")
 					}
 					$('.mychk').click(function(){
 						$(this).find('img').remove()
 						$(this).attr('checked' , false).show()
-						$(this).removeClass('mychk')
 					})
 				}
 			})
