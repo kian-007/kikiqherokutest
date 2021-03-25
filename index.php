0fe6f53b27b570d5c26a6c715e22e6b5ddd12a00
kian <king.kian007@gmail.com>
2021-03-26 00:12:41 +0430

    t

--- index.php
+++ index.php
@@ -179,14 +179,6 @@
 						<!-- Full-width images with number and caption text -->
 						<div class="mySlides fade">
 							<div class="numbertext">1 / 7</div>
-							<a href="https://kikiq.herokuapp.com/showimages.php?image=1">
-							<img src="images\kiki.jpg" style="border-radius: 35px; width:100%; height:550px">
-							</a>
-							<div class="text">KikiCutest</div>
-						</div>
-						
-						<div class="mySlides fade">
-							<div class="numbertext">2 / 7</div>
 							<a href="https://kikiq.herokuapp.com/showimages.php?image=2">
 							<img src="images\bags1.jpg" style="border-radius: 35px; width:100%; height:550px">
 							</a>
@@ -194,7 +186,7 @@
 						</div>
 						
 						<div class="mySlides fade">
-							<div class="numbertext">3 / 7</div>
+							<div class="numbertext">2 / 7</div>
 							<a href="https://kikiq.herokuapp.com/showimages.php?image=3">
 							<img src="images\bags2.jpg" style="border-radius: 35px; width:100%; height:550px">
                             </a>
@@ -202,7 +194,7 @@
 						</div>
 						
 						<div class="mySlides fade">
-							<div class="numbertext">4 / 7</div>
+							<div class="numbertext">3 / 7</div>
 							<a href="https://kikiq.herokuapp.com/showimages.php?image=4">
 							<img src="images\brushes.jpg" style="border-radius: 35px; width:100%; height:550px">
 							</a>
@@ -210,7 +202,7 @@
 						</div>
 						
 						<div class="mySlides fade">
-							<div class="numbertext">5 / 7</div>
+							<div class="numbertext">4 / 7</div>
 							<a href="https://kikiq.herokuapp.com/showimages.php?image=5">
 							<img src="images\mirror1.jpg" style="border-radius: 35px; width:100%; height:550px">
                             </a>
@@ -218,7 +210,7 @@
 						</div>
 						
 						<div class="mySlides fade">
-							<div class="numbertext">6 / 7</div>
+							<div class="numbertext">5 / 7</div>
 							<a href="https://kikiq.herokuapp.com/showimages.php?image=6">
 							<img src="images\mirror1.jpg" style="border-radius: 35px; width:100%; height:550px">
 							</a>
@@ -226,13 +218,20 @@
 						</div>
 						
 						<div class="mySlides fade">
-							<div class="numbertext">7 / 7</div>
+							<div class="numbertext">6 / 7</div>
 							<a href="https://kikiq.herokuapp.com/showimages.php?image=7">
 							<img src="images\mirror2.jpg" style="border-radius: 35px; width:100%; height:550px">
 							</a>
 							<div class="text">BAG's</div>
 						</div>
 						
+						<div class="mySlides fade">
+							<div class="numbertext">7 / 7</div>
+							<a href="https://kikiq.herokuapp.com/showimages.php?image=1">
+							<img src="images\kiki.jpg" style="border-radius: 35px; width:100%; height:550px">
+							</a>
+							<div class="text">KikiCutest</div>
+						</div>
 						  <!-- Next and previous buttons -->
 						<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 						<a class="next" onclick="plusSlides(1)">&#10095;</a>
