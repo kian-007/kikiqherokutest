211d75ce2922dd7b06c967ee80809092e260ed4e
kian <king.kian007@gmail.com>
2021-04-04 01:37:10 +0430

    biggg test

--- test2.php
+++ test2.php
@@ -1,6 +1,32 @@
 <?php 
 
 require_once ('config.php');
+<?php 
+function get_value($para, $default){
+    if(isset($_GET[$para])){
+        $value = $_GET[$para];
+    }else{
+        $value = $default;
+    }
+    return $value;
+}
 
+$facebrush = get_value('facebrush' , 0);
+$handcream = get_value('handcream' , 0);
+
+if($handcream ){
+    if($facebrush){
+        $title = 'فیس براش چکشی';
+    }else{
+        $title = 'کرم مرطوب کننده دست';
+    }
+}else{
+        if($facebrush){
+           $title = 'فیس براش چکشی';
+        }else{
+           $title = 'پوست';
+        }
+    }
+?>
 render_skin_page();
     
