3886e3624dc9199df00b12be8c459fa375865b9f
kian <king.kian007@gmail.com>
2021-04-04 23:26:34 +0430

    big testt

--- skin.php
+++ skin.php
@@ -1,33 +1,3 @@
-<?php 
-function get_value($para, $default){
-    if(isset($_GET[$para])){
-        $value = $_GET[$para];
-    }else{
-        $value = $default;
-    }
-    return $value;
-}
-
-$facebrush = get_value('facebrush' , 0);
-$handcream = get_value('handcream' , 0);
-
-if($handcream ){
-    if($facebrush){
-        $title = 'فیس براش چکشی';
-    }else{
-        $title = 'کرم مرطوب کننده دست';
-    }
-}else{
-        if($facebrush){
-           $title = 'فیس براش چکشی';
-        }else{
-           $title = 'پوست';
-        }
-    }
-?>
-
-
-
 <html>
 	<head>
 		<meta charset="UTF-8" />
