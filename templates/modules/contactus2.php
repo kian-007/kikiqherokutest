5c9cc155e68d49c95df72840672972334d2b5a50
kian <king.kian007@gmail.com>
2021-04-05 06:46:23 +0430

    t

--- templates/modules/contactus2.php
+++ templates/modules/contactus2.php
@@ -14,8 +14,8 @@ function get_content(){ ?>
 					<div id=pcontainer >
 						<div id="dimages">
 							<span style="font-size:15pt; position:relative; float:right; text-align:right;">KikiCutest را در شبکه های اجتماعی دنبال کنید</span>
-							<p class="mg" style="color:white; position: relative; top:50px">kikiqutest_online@<img style="float:right" class="imglogo" src="images\instagram.svg"  /></p>
-							<p class="mg" style="color:white; position: relative; top:70px">kikiqutest_online@<img style="clear:right; float:right;"class="imglogo" src="images\telegram.svg"  /></p>
+							<p class="mg" style="color:white; position: relative; top:50px">kikicutest_online@<img style="float:right" class="imglogo" src="images\instagram.svg"  /></p>
+							<p class="mg" style="color:white; position: relative; top:70px">kikicutest_online@<img style="clear:right; float:right;"class="imglogo" src="images\telegram.svg"  /></p>
 							<p class="mg" style="color:white; position: relative; top:90px">kikiqutest.online@gmail.com<img style="clear:right; float:right;" class="imglogo" src="images\email.svg"  /></p>
 							<p class="mg" style="color:white; position: relative; top:110px">phone > 09931556696</p>
 						</div>
