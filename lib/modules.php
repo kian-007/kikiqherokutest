8b88ad7ff030b705da6d447e63985d1fb299f3a5
kian <king.kian007@gmail.com>
2021-04-11 00:25:09 +0430

    update

--- lib/modules.php
+++ lib/modules.php
@@ -2,7 +2,7 @@
     function render_page(){
         include_once ('templates/header.php');
 
-        include_once ('functions.php');
+        include_once ('lib/functions.php');
         $module = get_module_name();
         include_once ('templates/modules/'.'m'."$module");
             get_content();
