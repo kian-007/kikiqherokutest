6d13c04bfaaed0e5576cfa1abe649df0d916ebcc
kian <king.kian007@gmail.com>
2021-04-11 20:17:47 +0430

    menue

--- templates/header.php
+++ templates/header.php
@@ -18,7 +18,7 @@ alert(d + '\n خوش آمدید')
 slert('User <?php   echo $_GET['REMOTE_ADDR'];	?> \n Wellcome To Kikiqutest')*/
 				$('.ulmenue').find('li:nth-child(6)').click(function(){ 
 					$(this).toggleClass('baz')
-					$('.ulmenue').find('li:nth-child(4)').prevAll().toggle(250)
+					$('.ulmenue').find('li:nth-child(6)').prevAll().toggle(250)
 					if($(this).hasClass('baz')){
 					$(this).css({'background-image': "url('images/threeline4.jpg')", 'background-size': "45px 41px"})
 					}else{
