a06c14f24332c12fa27c8c99367c7cf6ce92fe0d
kian <king.kian007@gmail.com>
2021-04-11 20:54:44 +0430

    update index

--- templates/footer.php
+++ templates/footer.php
@@ -5,5 +5,9 @@
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ff66b3" fill-opacity="1" d="M0,64L48,58.7C96,53,192,43,288,53.3C384,64,480,96,576,128C672,160,768,192,864,197.3C960,203,1056,181,1152,149.3C1248,117,1344,75,1392,53.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 			</div>
 		</div>
+		<?php  include_once ('lib/functions.php');
+                       $module = get_module_name();
+                       include_once ('templates/modules/'.'m'."$module");
+                       get_java_script(); ?>
 	</body>
 </html>
--- templates/modules/mindex.php
+++ templates/modules/mindex.php
@@ -101,3 +101,84 @@ function get_content(){ ?>
 				<div class="right">
 				</div>
 <?php }
+
+function get_style(){ ?>
+        <style>
+			spinner-border{to{transform:rotate(360deg)}}@keyframes spinner-border{to{transform:rotate(360deg)}}.spinner-border{display:inline-block;width:2rem;height:2rem;vertical-align:text-bottom;border:.25em solid currentColor;border-right-color:transparent;border-radius:50%;-webkit-animation:.75s linear infinite spinner-border;animation:.75s linear infinite spinner-border}.spinner-border-sm{width:1rem;height:1rem;border-width:.2em}@-webkit-keyframes spinner-grow{0%{transform:scale(0)}50%{opacity:1;transform:none}}@keyframes spinner-grow{0%{transform:scale(0)}50%{opacity:1;transform:none}}.spinner-grow{display:inline-block;width:2rem;height:2rem;vertical-align:text-bottom;background-color:currentColor;border-radius:50%;opacity:0;-webkit-animation:.75s linear infinite spinner-grow;animation:.75s linear infinite spinner-grow}.spinner-grow-sm{width:1rem;height:1rem}@media (prefers-reduced-motion:reduce){.spinner-border,.spinner-grow{-webkit-animation-duration:1.5s;animation-duration:1.5s}}
+			
+			#preloader
+			{
+				margin-left: 350px;
+			}
+			.ulmenue > li:nth-child(6)
+			{
+				background-image: url('images/threeline3.jpg');
+				background-size: 45px 41px;
+				background-repeat: no-repeat;
+				background-position: 85px 4px;
+				text-align: left;
+			}
+			.ulmenue > li:nth-child(6) span
+			{
+				line-height: 2.5;
+				position: relative;
+				left: 22px;
+				top: 3px;
+			}
+		</style>
+<?php }
+
+function get_java_script(){ ?>
+		<script type="text/javascript">
+			// var Index = 0;                    auto slide show
+			// showSlides();
+			
+			// function showSlides() {
+			// var i;
+			// var slides = document.getElementsByClassName("mySlides");
+			// for (i = 0; i < slides.length; i++) {
+				// slides[i].style.display = "none";
+			// }
+			// Index++;
+			// if (Index > slides.length) {Index = 1} 
+			// slides[Index-1].style.display = "block";
+			// setTimeout(showSlides, 3500); // Change image every 2 seconds
+			// }
+			
+			
+			
+			var slideIndex = 1;
+			showSlides(slideIndex);
+			
+			// Next/previous controls
+			function plusSlides(n) {
+			showSlides(slideIndex += n);
+			}
+			
+			// Thumbnail image controls
+			function currentSlide(n) {
+			showSlides(slideIndex = n);
+			}
+			
+			function showSlides(n) {
+			var i;
+			var slides = document.getElementsByClassName("mySlides");
+			var dots = document.getElementsByClassName("dot");
+			if (n > slides.length) {slideIndex = 1}
+			if (n < 1) {slideIndex = slides.length}
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";
+			}
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(" active", "");
+			}
+			slides[slideIndex-1].style.display = "block";
+			dots[slideIndex-1].className += " active";
+			}
+			
+			
+			
+			<!-- loading spinner -->
+			window.onload = function(){ document.getElementById("preloader").style.display = "none"; }
+		</script> 
+<?php }
