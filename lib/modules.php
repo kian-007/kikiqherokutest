bbdff1070b4c41748a4efa47c33452e11ce145c4
kian <king.kian007@gmail.com>
2021-04-12 20:52:33 +0430

    file exists

--- lib/modules.php
+++ lib/modules.php
@@ -4,9 +4,9 @@
 
         include_once ('lib/functions.php');
         $module = get_module_name();
-		$module_file = 'templates/modules/'.'m'."$module";
-		if(file_exists($module_file){
-			include_once ('templates/modules/'.'m'."$module");
+		$module_file = "templates/modules/m$module";
+		if(file_exists($module_file)){
+			include_once ("templates/modules/m$module");
 		}else{
 			include_once ('templates/modules/mindex.php');
 		}
