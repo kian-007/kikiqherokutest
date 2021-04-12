7792735999c480ef19f8606d7d9f462e6b8c7a07
kian <king.kian007@gmail.com>
2021-04-12 22:24:30 +0430

    contact

--- templates/modules/mcontactus.php
+++ templates/modules/mcontactus.php
@@ -15,7 +15,7 @@ function get_content(){ ?>
 						<div id="dimages">
 							<span style="font-size:15pt; position:relative; float:right; text-align:right;">KikiCutest را در شبکه های اجتماعی دنبال کنید</span>
 							<p class="mg" style="color:white; position: relative; top:50px"><a href="https://www.instagram.com/kikicutest_online/">kikicutest_online@</a><img style="float:right" class="imglogo" src="images\instagram.svg"  /></p>
-							<p class="mg" style="color:white; position: relative; top:70px"><a href="https://www.telegram.com/kikicutest_online/">kikicutest_online@</a><img style="clear:right; float:right;"class="imglogo" src="images\telegram.svg"  /></p>
+							<p class="mg" style="color:white; position: relative; top:70px"><a href="https://t.me/kikicutest_online">kikicutest_online@</a><img style="clear:right; float:right;"class="imglogo" src="images\telegram.svg"  /></p>
 							<p class="mg" style="color:white; position: relative; top:90px"><a href="https://mail.google.com/mail/u/1/#inbox">kikiqutest.online@gmail.com</a><img style="clear:right; float:right;" class="imglogo" src="images\email.svg"  /></p>
 							<p class="mg" style="color:white; position: relative; top:110px">phone > 09931556696</p>
 						</div>
@@ -34,6 +34,7 @@ function get_style(){ ?>
 				position:relative;
 				top: 100px;
 				background-color:#dddddd;
+				width: 500px;
 				height: 500px;
 				border: solid black 3px;
 				border-top-left-radius:35px;
