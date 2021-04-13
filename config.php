927e5b2b939a8f7a910b5bd01279fb688eb6d7e6
kian <king.kian007@gmail.com>
2021-04-13 09:55:37 +0430

    check

--- config.php
+++ config.php
@@ -10,5 +10,30 @@ foreach(glob('lib/*.php') as $lib_file){
     include_once ($lib_file);
 }
 
+global $db;
+$db = new SQLite3(DB_FILENAME);
+
+function create_db_tables(){
+	global $db;
+	$db->query("
+		CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT NOT NULL,
+		password TEXT NOT NULL,
+		first_name TEXT NOT NULL,
+		last_name TEXT NOT NULL
+		);
+	");
+	
+	$db->query("
+		CREATE TABLE IF NOT EXISTS options(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		option_name TEXT NOT NULL,
+		option_value TEXT NOT NULL
+		);
+	");
+	
+	
+}
 
 
