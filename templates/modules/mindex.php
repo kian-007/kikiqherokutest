767270cd99bbff67262e124ef7d92ac0832e31f7
kian <king.kian007@gmail.com>
2021-04-14 19:04:49 +0430

    postgresql

--- templates/modules/mindex.php
+++ templates/modules/mindex.php
@@ -95,11 +95,10 @@ function get_content(){ ?>
 					</div>
 					
 					<a href="#start" style="position:absolute; text-decoration: none; ">up<img	style="border-radius:45px" src="images/up2.jpg"	/></a>
-				<br/>
-				<br/>
-				<br/>
-		   		<?php   $url = 'https://kikiq.herokuapp.com/';
-						var_dump(parse_url($url)); ?>
+
+				<?php 
+		   		$db = parse_url(getenv("DATABASE_URL"));
+				$db["path"] = ltrim($db["path"], "/"); ?>
 				</div>
 				<div class="right">
 				</div>
