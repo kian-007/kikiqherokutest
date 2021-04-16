94d8950085e9c6a2de4c72f7d5f09ec61c804248
kian <king.kian007@gmail.com>
2021-04-17 01:54:02 +0430

    login

--- templates/modules/mlogin.php
+++ templates/modules/mlogin.php
@@ -3,35 +3,44 @@
 function get_title(){
     return 'ورود به صفحه کاربری';
 }
+
 function get_content(){ ?>
 
-<div class="row mb-3">
-      <div class="col-md-4 themed-grid-col"></div>
-      <div class="col-md-4 themed-grid-col">
-          
-          <br/>
-          <br/>
-          <main class="form-signin">
-              <form    method="post">
-                  <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                <div class="left2">
+				</div>
 
-                  <div class="form-floating">
-                        <input class="form-control" id="username" name='username' placeholder="username">
-                        <label for="floatingInput">Username</label>
-                  </div>
-                  <div class="form-floating">
-                        <input type="password" class="form-control" id="password" name='password' placeholder="Password">
-                        <label for="floatingPassword">Password</label>
-                  </div>
-                  <br/>
-                  <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                  <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-                </form>
-          </main>
-          
-      </div>
-      <div class="col-md-4 themed-grid-col"></div>
-</div>
+				<div class="content2">
+                <div class="row mb-3">
+                      <div class="col-md-4 themed-grid-col"></div>
+                      <div class="col-md-4 themed-grid-col">
+                          
+                          <br/>
+                          <br/>
+                          <main class="form-signin">
+                              <form    method="post">
+                                  <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                
+                                  <div class="form-floating">
+                                        <input class="form-control" id="username" name='username' placeholder="username">
+                                        <label for="floatingInput">Username</label>
+                                  </div>
+                                  <div class="form-floating">
+                                        <input type="password" class="form-control" id="password" name='password' placeholder="Password">
+                                        <label for="floatingPassword">Password</label>
+                                  </div>
+                                  <br/>
+                                  <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                                  <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+                                </form>
+                          </main>
+                          
+                      </div>
+                      <div class="col-md-4 themed-grid-col"></div>
+                </div>
+				</div>
+				
+				<div class="right2">
+				</div>
 <?php }
 
 function get_style(){ ?>
