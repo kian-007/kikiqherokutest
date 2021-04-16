1c66aa4bb2cc29661e0a8c5178f2ae8f0422b993
kian <king.kian007@gmail.com>
2021-04-17 02:23:38 +0430

    form

--- templates/modules/mlogin.php
+++ templates/modules/mlogin.php
@@ -10,35 +10,22 @@ function get_content(){ ?>
 				</div>
 
 				<div class="content2">
-                <div class="row mb-3">
-                      <div class="col-md-4 themed-grid-col"></div>
-                      <div class="col-md-4 themed-grid-col">
-
-                          <br/>
-                          <br/>
-                          <main class="form-signin">
-                              <form    method="post">
-                                  <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                                  <div class="form-floating">
-                                        <input class="form-control" id="username" name='username' placeholder="username">
-                                        <label for="floatingInput">Username</label>
-                                  </div>
-                                  <div class="form-floating">
-                                        <input type="password" class="form-control" id="password" name='password' placeholder="Password">
-                                        <label for="floatingPassword">Password</label>
-                                  </div>
-                                  <br/>
-                                  <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                                  <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-                                </form>
-                          </main>
-
-                      </div>
-                      <div class="col-md-4 themed-grid-col"></div>
-                </div>
-				</div>
-				
+                    <div class="dform">
+                        <form    method="post">
+                            <h1>Please sign in</h1>
+                            <div>
+                                  <input class="form-control" id="username" name='username' placeholder="username">
+                                  <label for="username">Username</label>
+                            </div>
+                            <div>
+                                  <input type="password" id="password" name='password' placeholder="Password">
+                                  <label for="password">Password</label>
+                            </div>
+                            <br/>
+                            <button type="submit">Sign in</button>
+                            <p>&copy; 2017–2021</p>
+                        </form>
+                    </div>
 				<div class="right2">
 				</div>
 <?php }
@@ -53,6 +40,5 @@ function css_link(){ ?>
 
 <link type="text/css"	rel="stylesheet"	href="index.css" />
 <link type="text/css"	rel="stylesheet"	href="index3.css" />
-<link href="bootstrap.min.css" rel="stylesheet"/>
 
 <?php }
