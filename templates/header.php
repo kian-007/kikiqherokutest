ee39d4c9a85f72bc1684405a0981c3da6dbbc054
kian <king.kian007@gmail.com>
2021-04-17 11:20:31 +0430

    css

--- templates/header.php
+++ templates/header.php
@@ -96,7 +96,7 @@ slert('User <?php   echo $_GET['REMOTE_ADDR'];	?> \n Wellcome To Kikiqutest')*/
 				$('.ulmenue').children('li').eq(0).nextUntil(':last-child').css('width', "75px")
 				$('.ulmenue').children('li').eq(0).nextUntil(':last-child').css('background-position', "50px 15px")
 				$('.ulmenue').children('li').first().css('width', "160px")
-				$('.ulmenue').children('li').first().css('background-position', "60px 15px")
+				$('.ulmenue').children('li').first().css('background-position', "130px 15px")
 				$('.ulmenue').children('li').eq(5).css('background-position', "40px 10px")
 
 			})
