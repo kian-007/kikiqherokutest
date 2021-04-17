562c55187861d20910eafd27b75261cf157fbd9b
kian <king.kian007@gmail.com>
2021-04-17 14:05:55 +0430

    css

--- templates/modules/mlogin.php
+++ templates/modules/mlogin.php
@@ -2,10 +2,29 @@
                                     return 'ورود به صفحه کاربری';
                                 }
                                 
-                                function get_content() {
-                                    ?><div class="left2"></div><div class="content2"><div class="dform"><form method="post"><h1>Please sign in</h1><div class="dinput"><input class="input second" id="username" name='username' placeholder="username" /><label for="username">Username</label></div><div class="dinput"><input class="input second" type="password" id="password" name='password' placeholder="Password" /><label for="password">Password</label></div><br/><button class="first" type="submit">Sign in</button><p style="margin-left: 170px;">&copy;
-                                    2017–2021</p></form></div><div class="right2"></div><?php
-                                }
+                                function get_content() {?>
+                                    <div class="left2">
+				                    </div>
+				                    <div class="content2">
+                                        <div class="dform">
+                                            <form    method="post">
+                                                <h1>Please sign in</h1>
+                                                <div class="dinput">
+                                                      <input class="input second" id="username" name='username' placeholder="username" />
+                                                      <label for="username">Username</label>
+                                                </div>
+                                                <div class="dinput">
+                                                      <input class="input second" type="password" id="password" name='password' placeholder="Password" />
+                                                      <label for="password">Password</label>
+                                                </div>
+                                                <br/>
+                                                <button class="first" type="submit">Sign in</button>
+                                                <p style="margin-left: 170px;">&copy; 2017–2021</p>
+                                            </form>
+                                        </div>
+				                    <div class="right2">
+				                    </div>
+                                <?php}
                                 
                                 function get_style() {
                                     ?><style>h1 {
