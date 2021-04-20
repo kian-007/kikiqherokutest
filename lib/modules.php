16da50f6ab7bab4b4d9e390f46ebfbd8c6e869e8
kian <king.kian007@gmail.com>
2021-04-20 11:39:45 +0430

    load_module

--- lib/modules.php
+++ lib/modules.php
@@ -1,16 +1,24 @@
 <?php   
+
+	function load_module(){
+		include_once ('lib/functions.php');
+        $module = get_module_name();
+		$module_file = "templates/modules/m$module";
+
+		if(file_exists($module_file)){
+			require_once ("templates/modules/m$module");
+		}else{
+			require_once ('templates/modules/mindex.php');
+		}
+		get_content();
+	}
+
+
+
     function render_page(){
         include_once ('templates/header.php');
 
-        include_once ('lib/functions.php');
-        $module = get_module_name();
-		$module_file = "templates/modules/m$module";
-		if(file_exists($module_file)){
-			include_once ("templates/modules/m$module");
-		}else{
-			include_once ('templates/modules/mindex.php');
-		}
-		get_content();
-		
-	include_once('templates/footer.php'); 
+        load_module();
+
+		include_once('templates/footer.php'); 
 }
