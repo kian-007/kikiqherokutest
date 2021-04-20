dfe3993cf31ec95d3e4a8a56e3b1d9044e7d1935
kian <king.kian007@gmail.com>
2021-04-20 11:47:03 +0430

    modules

--- lib/modules.php
+++ lib/modules.php
@@ -3,14 +3,21 @@
 	function load_module(){
 		include_once ('lib/functions.php');
         $module = get_module_name();
+		if(empty($module)){
+			$module = 'index.php';
+		}
 		$module_file = "templates/modules/m$module";
 
 		if(file_exists($module_file)){
 			require_once ("templates/modules/m$module");
 		}else{
-			require_once ('templates/modules/mindex.php');
+
+			//require_once ('templates/modules/mindex.php');
 		}
-		get_content();
+
+		if(function_exists(get_content)){
+			get_content();
+		}
 	}
 
 
