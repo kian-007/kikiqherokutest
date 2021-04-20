6b39dd9536614f363cae7931f4d3cf1c23f1610b
kian <king.kian007@gmail.com>
2021-04-20 12:26:32 +0430

    load modules

--- lib/modules.php
+++ lib/modules.php
@@ -24,8 +24,8 @@
         include_once ('templates/header.php');
 
         load_module();
-		if(!function_exists('get_content'){
-			function get_content(){}
+		if(function_exists('get_content'){
+			get_content();
 		}
 		get_content();
 
