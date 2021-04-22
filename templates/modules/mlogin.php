61f8326b3f35555b47cb6113f767c6951aff2e8a
kian <king.kian007@gmail.com>
2021-04-22 22:42:33 +0430

    fix grid

--- templates/modules/mlogin.php
+++ templates/modules/mlogin.php
@@ -3,12 +3,12 @@
                                 }
                                 
                                 function get_content() {?>
-                                <div class="left2">
+                                <div class="left2 col-1">
                                 </div>
-                                <div class="content2"><div class="dform"><form method="post"><h1>Please sign in</h1><div class="dinput"><input class="input second" id="username" name='username' placeholder="username" /><label for="username">Username</label></div><div class="dinput"><input class="input second" type="password" id="password" name='password' placeholder="Password" /><label for="password">Password</label></div><br/><button class="first" type="submit">Sign in</button><p style="margin-left: 170px;">&copy;
+                                <div class="content2 col-9"><div class="dform"><form method="post"><h1>Please sign in</h1><div class="dinput"><input class="input second" id="username" name='username' placeholder="username" /><label for="username">Username</label></div><div class="dinput"><input class="input second" type="password" id="password" name='password' placeholder="Password" /><label for="password">Password</label></div><br/><button class="first" type="submit">Sign in</button><p style="margin-left: 170px;">&copy;
                                     2017–2021</p></form></div>
                                 </div>
-                                <div class="right2">
+                                <div class="right2 col-2">
                                 </div>
                                     <?php
                                 }
@@ -91,6 +91,9 @@
                                         }
                                         </style><?php
                                     }
-                                    function css_link() {
-                                        ?><link type="text/css" rel="stylesheet" href="index.css" /><link type="text/css" rel="stylesheet" href="index3.css" /><?php
-                                    }
+                                    function css_link() {?>
+
+                                        <link type="text/css" rel="stylesheet" href="index.css" />
+                                        <link type="text/css" rel="stylesheet" href="index3.css" />
+
+                                    <?php}
