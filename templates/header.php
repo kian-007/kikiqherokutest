30e15524c3ad79cbd20b52e6111e3fd049efe84b
kian <king.kian007@gmail.com>
2021-04-30 03:30:23 +0430

    rwd

--- templates/header.php
+++ templates/header.php
@@ -91,12 +91,12 @@ slert('User <?php   echo $_GET['REMOTE_ADDR'];	?> \n Wellcome To Kikiqutest')*/
 				})
 				
 				$('.secondulmenue').children('li').click(function() {
-					$(this).toggleClass('showarrow')
+					$(this).addClass('showarrow')
 					$(this).find('.subul2').addClass('current')
 					$(this).find('.subul2').slideToggle()
 					$('.subul2').not('.current').slideUp()
 					$(this).find('.subul2').removeClass('current')
-					
+					$(this).removeClass('showarrow')
 				})
 
 				$('.secondulmenue').children('li').hover(function(){
