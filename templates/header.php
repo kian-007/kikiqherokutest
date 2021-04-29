40fa46d28ea8d8d935199eadb20702095dc37765
kian <king.kian007@gmail.com>
2021-04-30 01:12:08 +0430

    rwd

--- templates/header.php
+++ templates/header.php
@@ -77,9 +77,8 @@ slert('User <?php   echo $_GET['REMOTE_ADDR'];	?> \n Wellcome To Kikiqutest')*/
 				// for header2 -------------------------
 				$('.ulmenue2').find('li:nth-child(2)').click(function(){ 
 					$('.showmenue').fadeToggle()
-					$('.showmenue').addClass('open')
 
-					if($('.showmenue').hasClass('open')){
+					if(document.getElementsByClassName('.showmenue').display == 'block'){
 						$('body').css('background-color', "rgba(0,0,0,0.5")
 						$('.footer').css('background-color', "rgba(0,0,0,0.5")
 						$('.middle').css('-webkit-filter', "blur(10px)")
