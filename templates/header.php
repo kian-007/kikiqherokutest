a74c2fdb0ee99eb0bfefba18688d7dc95b9b1814
kian <king.kian007@gmail.com>
2021-04-29 23:02:22 +0430

    rwd

--- templates/header.php
+++ templates/header.php
@@ -28,7 +28,7 @@ var d = d.toDateString();
 alert(d + '\n خوش آمدید')
 slert('User <?php   echo $_GET['REMOTE_ADDR'];	?> \n Wellcome To Kikiqutest')*/
 				$('.ulmenue').children().addClass('prevli')
-				$('.secondmenue').children().addClass('prevli2')
+				$('.secondmenue').children('li').addClass('prevli2')
 				$('.ulmenue').find('li').last().removeClass('prevli')
 				$('.ulmenue').find('li').last().prev().removeClass('prevli')
 				
