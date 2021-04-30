b7be790e31ce0512f9752d2e4d72d85b87b4a2a0
kian <king.kian007@gmail.com>
2021-04-30 22:39:14 +0430

    rwd

--- templates/modules/mlogin.php
+++ templates/modules/mlogin.php
@@ -2,7 +2,7 @@
     function get_title() {
         return 'ورود به صفحه کاربری';
     }
-    
-    function get_content() {?>
+
+    function get_content(){?>
         <p>This is a Test</p>
-    <?php }
+    <?php}
