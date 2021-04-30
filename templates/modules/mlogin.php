9ece7768b3b5668757b6d607a7cd4776a76faf1e
kian <king.kian007@gmail.com>
2021-05-01 00:11:10 +0430

    rwd login

--- templates/modules/mlogin.php
+++ templates/modules/mlogin.php
@@ -92,7 +92,8 @@ function get_style(){ ?>
             color: #03e9f4; font-size: 14px;
         }
         .input ~label{
-            margin-top: -200px;
+            top: -100px;
+            margin-top: 0px;
             margin-left: 100px;
         }
 
