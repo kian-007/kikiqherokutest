b06665d43cc923f1f0ae01c09b138842f6452d39
kian <king.kian007@gmail.com>
2021-04-30 22:51:18 +0430

    login issue

---
+++ templates/modules/mlogin.php
@@ -0,0 +1,18 @@
+<?php
+function get_title(){
+    return 'ورود به صفحه کاربری';
+}
+
+function get_content(){ ?>
+	<p>This is a Test</p>
+<?php }
+
+function get_style(){ ?>
+    <style>
+          p{
+              background-color: yellow;
+              width: 100px;
+              height: 100px;
+          }
+    </style>
+<?php }
