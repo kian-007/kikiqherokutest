bc22ca7517bf1b78e4df5f8b3813a4b9035237e6
kian <king.kian007@gmail.com>
2021-05-01 06:05:02 +0430

    t

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,5 @@
 <?php 
 
 require_once ('config.php');
-if(is_authentication_required()){
-    $login_url = home_url('login.php');
-    header("Location: $login_url");
-    die();
-}
+
 render_page();
--- lib/modules.php
+++ lib/modules.php
@@ -29,6 +29,11 @@
         include_once ('templates/header.php');
 
         load_module();
+		if(is_authentication_required()){
+			$login_url = home_url('login.php');
+			header("Location: $login_url");
+			die();
+		}
 		if(function_exists('get_content')){
 			get_content();
 		}
