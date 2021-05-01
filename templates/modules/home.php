1c4b6ac09bea1ca551f43099a365310b69e59045
kian <king.kian007@gmail.com>
2021-05-02 00:32:51 +0430

    add images

--- templates/modules/home.php
+++ templates/modules/home.php
@@ -10,6 +10,10 @@ function get_content(){ ?>
 				<div class="left2 col-l-1 col-12">
 				</div>
 				<div class="content2 col-l-9 col-12">
+
+				<p class="title"><span>جدید ترین محصولات</span></p>
+
+
 				<div id='preloader'>
 					<div class="text-center">
 						Loading...
@@ -19,8 +23,8 @@ function get_content(){ ?>
 					</div>
 				</div>
 				<br/>
+				
 					<div class="slideshow-container">
-					
 						<!-- Full-width images with number and caption text -->
 						<div class="mySlides fade">
 							<div class="numbertext">1 / 11</div>
@@ -142,6 +146,14 @@ function get_style(){ ?>
         <style>
 			spinner-border{to{transform:rotate(360deg)}}@keyframes spinner-border{to{transform:rotate(360deg)}}.spinner-border{display:inline-block;width:2rem;height:2rem;vertical-align:text-bottom;border:.25em solid currentColor;border-right-color:transparent;border-radius:50%;-webkit-animation:.75s linear infinite spinner-border;animation:.75s linear infinite spinner-border}.spinner-border-sm{width:1rem;height:1rem;border-width:.2em}@-webkit-keyframes spinner-grow{0%{transform:scale(0)}50%{opacity:1;transform:none}}@keyframes spinner-grow{0%{transform:scale(0)}50%{opacity:1;transform:none}}.spinner-grow{display:inline-block;width:2rem;height:2rem;vertical-align:text-bottom;background-color:currentColor;border-radius:50%;opacity:0;-webkit-animation:.75s linear infinite spinner-grow;animation:.75s linear infinite spinner-grow}.spinner-grow-sm{width:1rem;height:1rem}@media (prefers-reduced-motion:reduce){.spinner-border,.spinner-grow{-webkit-animation-duration:1.5s;animation-duration:1.5s}}
 			
+			.title{
+				margin: 50px auto;
+				width: 200px;
+				border-bottom: solid black 1px;
+			}
+			.title span{
+				border-bottom: solid black 3px;
+			}
 			#preloader
 			{
 				margin-left: 350px;
