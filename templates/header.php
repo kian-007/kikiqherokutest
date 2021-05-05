5efc69af32c6b1e7e3bb859c279d846854f83e33
kian <king.kian007@gmail.com>
2021-05-05 19:52:29 +0430

    showmenue blur

--- templates/header.php
+++ templates/header.php
@@ -75,6 +75,9 @@ slert('User <?php   echo $_GET['REMOTE_ADDR'];	?> \n Wellcome To Kikiqutest')*/
 				$('.ulmenue2').find('li:nth-child(2)').click(function(){ 
 					$('.showmenue').fadeToggle()
 					$('.showmenue').toggleClass('show')
+					$('.showmenue').blur(function(){
+						$(this).css('display', "none")
+					})
 
 					if($('.showmenue').hasClass('show')){
 						$('body').css('background-color', "rgba(0,0,0,0.5)")
