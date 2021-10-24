fb8c0babb69b1f55287561e99facd375114fbede
kian <king.kian007@gmail.com>
2021-10-24 23:16:09 +0330

    dashboard

--- templates/modules/dashboard.php
+++ templates/modules/dashboard.php
@@ -13,7 +13,7 @@ function authentication_required(){
 function get_content(){ ?>
 
 	<?php $current_user = get_current_user_data(); ?>
-	<h3 style="margin-left: 500px;"><?php echo $current_user['username']; ?></h3>
+	<h3 style="margin-left: 666px;"><?php echo $current_user['username']; ?></h3>
 	<div class="left2 col-l-1 col-12">
     </div>
     <div class="content2 col-l-9 col-12">
@@ -114,7 +114,8 @@ function process_inputs(){
 				border-radius: 30px;
 				width: 420px;
 				height: 350px;
-				padding-right: 50px;
+				padding-right: 100px;
+				padding-left: 100px;
 				box-shadow: 7px 7px 10px rgba(0, 0, 0, 0.7), -7px -7px 7px rgb(255, 255, 255) ;
 			}
 			button {
