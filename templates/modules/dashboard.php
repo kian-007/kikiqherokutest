24c1f384831a7260bfac75612d9c4df7d90a02e0
kian <king.kian007@gmail.com>
2021-10-24 22:44:41 +0330

    update

--- templates/modules/dashboard.php
+++ templates/modules/dashboard.php
@@ -1,45 +1,113 @@
 <?php
 
-function authentication_required(){
-	return true;
-}
 
 function get_title(){
-    return 'صفحه کاربری';
+    echo 'پروفایل';
+}
+
+
+function authentication_required(){
+    return true;
 }
 
 function get_content(){ ?>
 
-				
-				<div class="left2 col-l-1 col-12">
-				</div>
+	<?php $current_user = get_current_user_data(); ?>
+	<h3><?php echo $current_user['username']; ?></h3>
+	<div class="left2 col-l-1 col-12">
+    </div>
+    <div class="content2 col-l-9 col-12">
 
-				<div class="content2 col-l-9 col-12 clearfix">
-                    			<div>
-				        	<p id ='p1'> این صفحه فقط به کاربران نمایش داده میشود. </p>
-                    			</div>
-				</div>
-				
-				<div class="right2 col-l-2 col-12">
-				</div>
+        <div class="dform">
+            <form method="post">
+                <h1>Please sign in</h1>
+                <div class="dinput">
+                    <?php
+                        $username ='';
+                        if(isset($_POST['username'])){
+                            $username = $_POST['username'];
+                        }  
+                     ?>
+                    <input class="input second" id="new_username" name='new_username' placeholder="username" />
+                    <label for="new_username">نام کاربری جدید:</label>
+                </div>
+                <div class="dinput">
+                    <input class="input second" type="password" id="password" name='password' placeholder="Password" />
+                    <label for="password">رمز عبور جدید:</label>
+                </div>
+                <br/>
+                <button class="first" type="submit" name="save">Save</button>
+                <p style="margin-left: 140px;">&copy;2017–2021</p>
+            </form>
+        </div>
+
+    </div>
+    <div class="right2 col-l-2 col-12">
+    </div>
+
 <?php }
 
-function get_style(){ ?>
-    <style>
-        #p1
-        {
-            margin-top: 100px;
-            background-color: gray;
-            height: 25px;
-	        border-radius: 35px;
-	        text-align: right;
+
+
+
+function process_inputs(){
+    
+    //    if(is_user_logged_in()){
+    //        redirect_to(home_url());
+    //    }
+        
+        if(!isset($_POST['save'])){
+            return;
         }
-    </style>
-<?php }
+        
+        $current_user = get_current_user_data();
+        $username = $current_user['username'];
+        $_POST['username'] = $username;
+        
+        if(isset($_POST['password'])){
+            $password = $_POST['password'];
+        }
 
-function css_link(){ ?>
+        if(isset($_POST['new_username'])){
+            $new_username = $_POST['new_username'];
+        }
+        
 
-<link type="text/css"	rel="stylesheet"	href="index.css" />
-<link type="text/css"	rel="stylesheet"	href="index3.css" />
+        if(empty($new_username)){
+            add_message('نام کاربری جدید نمیتواند خالی باشد', 'error');
+            return;
+        }
 
-<?php }
+        if(empty($password)){
+            add_message('رمز عبور نمیتواند خالی باشد!', 'error');
+            return;
+        }
+        
+        if(user_exists($new_username)){
+            add_message('این نام کاربری قبلا انتخاب شده است!', 'warning');
+            return;
+        }
+
+        update_user($_POST);
+        
+        $user = get_user($new_username);
+        if($user){
+            add_message('تغیرات جدید با موفقیت ذخیره شد', 'info');
+            user_login($new_username, $password);
+        } else {
+            add_message('خطا!', 'error');
+        }
+
+        
+    }
+
+
+
+
+
+	function css_link(){ ?>
+
+		<link type="text/css"	rel="stylesheet"	href="index.css" />
+		<link type="text/css"	rel="stylesheet"	href="index3.css" />
+		
+	<?php }
