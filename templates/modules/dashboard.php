d5cffe65e39358a08fd6a2b1f0e72284a6296f46
kian <king.kian007@gmail.com>
2021-10-24 22:47:45 +0330

    update

--- templates/modules/dashboard.php
+++ templates/modules/dashboard.php
@@ -103,6 +103,109 @@ function process_inputs(){
 
 
 
+	function get_style(){ ?>
+		<style>
+			h1 {
+				margin-left: 75px;
+			}
+			.dform {
+				background: #d1bbbb;
+				margin: 0px auto;
+				border-radius: 30px;
+				width: 350px;
+				height: 350px;
+				box-shadow: 7px 7px 10px rgba(0, 0, 0, 0.7), -7px -7px 7px rgb(255, 255, 255) ;
+			}
+			button {
+				margin-left: 35px;
+				width: 200px;
+				height: 50px;
+				background: rgb(91, 33, 182);
+				color: #fff;
+				position: relative;
+				margin-bottom: 25px;
+				border-radius: 32px;
+				text-align: center;
+				cursor: pointer;
+			}
+			button:hover {
+				background-color: rgb(202, 67, 168);
+			}
+			.input {
+				margin-left: -50px;
+				width: 200px;
+				height: 50px;
+				background: #d1bbbb;
+				position: relative;
+				margin-bottom: 25px;
+				border-radius: 32px;
+				text-align: center;
+			}
+			.input::placeholder {
+				line-height: 100px;
+				font-family: Arial, Helvetica, sans-serif;
+				font-size: 32px;
+				font-weight: semibold;
+				z-index: 6;
+			}
+			.first {
+				box-shadow: 6px 6px 10px rgba(255, 255, 255, 0.8), -6px -6px 10px rgba(0, 0, 0, 0.2);
+				color: white;
+			}
+			.second {
+				box-shadow: inset -4px -4px 7px rgb(255, 255, 255), inset 8px 8px 7px rgba(0, 0, 0, 0.5);
+				color: gray;
+			}
+			input :hover {
+				border-top-color: blue;
+				border-left-color: blue;
+				border-right-color: blue;
+			}
+			.input:focus ~label{
+				margin-top: -25;
+				margin-left: 0px;
+				color: #03e9f4; font-size: 14px;
+			}
+	
+			@media only screen and (max-width: 768px){
+				.input ~label{
+					position: relative;
+					margin-top: -25;
+					left: -115px;
+					z-index: 7;
+					transition: .5s;
+				}
+			}
+	
+	
+			@media only screen and (min-width: 768px){
+				.dform{
+					width: 375px;
+				}
+				.input{
+					margin-left: 95px;
+				}
+				.input:focus ~label{
+					margin-top: -25;
+					margin-left: 0px;
+					color: #03e9f4; font-size: 14px;
+				}
+				.input ~label{
+					position: relative;
+					margin-top: -25;
+					left: 15px;
+					z-index: 7;
+					transition: .5s;
+				}
+				button{
+					margin-left: 100px;
+				}
+			}
+	
+	
+		</style>
+	<?php }
+
 
 
 	function css_link(){ ?>
