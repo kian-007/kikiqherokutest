e43a7cd7f0ea833e831d11a6e9a918bc72a8bb88
kian <king.kian007@gmail.com>
2021-11-24 03:55:33 +0330

    dargah

--- templates/modules/iteminfo.php
+++ templates/modules/iteminfo.php
@@ -564,7 +564,7 @@ function get_content(){ ?>
                     </p>
                     <p class="showitem-details">
                         <span><b>نام محصول:</b> <?php echo $item_title; ?> </span> <br/>
-                        <span><b>قیمت:</b> <?php echo $item_price; ?> </span> <br/>
+                        <span id="price"><b>قیمت:</b> <?php echo $item_price; ?> </span> <br/>
                         <span><b>دسته بندی:</b> <?php echo $categury; ?> </span> <br/>
                         <span><b>عدد موجود در انبار:</b> <?php echo $max; ?> </span> <br/>
                         <span><b>رنگ بندی:</b> - </span> <br/>
@@ -607,7 +607,7 @@ function get_content(){ ?>
                                 <input type="number" id="number" name="number" min="1" max="<?php echo $max; ?>" value="<?php echo $_POST['number']; ?>"/>
                             </td>
                             <td>
-                                <span id="price"><?php echo $item_price; ?></span>
+                                <span><?php echo $item_price; ?></span>
                             </td>
                             <td>
                                 <span><?php echo $off; ?></span>
@@ -804,10 +804,10 @@ function get_script(){ ?>
                 num = (1*num) + 1
                 $('#number').val(num)
 
-                var price = $('#price').text()
-                price = price.replace(" تومان", "")
-                price = price.replace(",", "")
-                var main_price = (price*1)
+                var main_price = $('#price').text()
+                main_price = main_price.replace(" تومان", "")
+                main_price = main_price.replace(",", "")
+                main_price = (main_price*1)
 
                 var final_price = main_price * num
                 final_price = final_price + " تومان"
