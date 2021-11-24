bae087db1f5156c4c6f12ab3c4a42bd9dd35eff5
kian <king.kian007@gmail.com>
2021-11-24 22:53:02 +0330

    dargah

--- testt.php
+++ testt.php
@@ -3,7 +3,7 @@ $url = "https://gateway.zibal.ir/v1/request";
 $curl = curl_init();
 
 curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HEADER, false);
 
 $data = curl_exec($curl);
