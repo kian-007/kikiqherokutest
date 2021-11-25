91cc68bdc84e5d8de3b001b269e38c360a5b4981
kian <king.kian007@gmail.com>
2021-11-25 21:32:19 +0330

    dargah

--- templates/modules/iteminfo.php
+++ templates/modules/iteminfo.php
@@ -790,30 +790,30 @@ function process_inputs(){
         $price = intval($price."0");
         echo $price.'process'.'<br/>';
 
-//        $number = intval($_POST['number']);
-//        $final_price = ($price*$number);
-//        $current_user = get_current_user_data();
-//        $phone_number = $current_user['phone_number'];
-//
-//        $parameters = array(
-//            "merchant"=> ZIBAL_MERCHANT_KEY,//required
-//            "callbackUrl"=> ZIBAL_CALLBACK_URL,//required
-//            "amount"=> $final_price,//required
-//            "orderId"=> time(),//optional
-//            "mobile"=> $phone_number,//optional for mpg
-//        );
-//
-//        $response = postToZibal('request', $parameters);
-//        var_dump($response);
-//        if ($response->result == 100)
-//        {
-//            $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
-//            header('location: '.$startGateWayUrl);
-//        }
-//        else{
-//            echo "errorCode: ".$response->result."<br>";
-//            echo "message: ".$response->message;
-//        }
+        $number = intval($_POST['number']);
+        $final_price = ($price*$number);
+        $current_user = get_current_user_data();
+        $phone_number = $current_user['phone_number'];
+
+        $parameters = array(
+            "merchant"=> ZIBAL_MERCHANT_KEY,//required
+            "callbackUrl"=> ZIBAL_CALLBACK_URL,//required
+            "amount"=> $final_price,//required
+            "orderId"=> time(),//optional
+            "mobile"=> $phone_number,//optional for mpg
+        );
+
+        $response = postToZibal('request', $parameters);
+        var_dump($response);
+        if ($response->result == 100)
+        {
+            $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
+            header('location: '.$startGateWayUrl);
+        }
+        else{
+            echo "errorCode: ".$response->result."<br>";
+            echo "message: ".$response->message;
+        }
 
 
     }else{
