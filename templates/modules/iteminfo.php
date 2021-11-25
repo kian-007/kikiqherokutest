3807c02bb5dfc03df04055455a9f16c2d2106d90
kian <king.kian007@gmail.com>
2021-11-25 04:51:50 +0330

    dargah

--- templates/modules/iteminfo.php
+++ templates/modules/iteminfo.php
@@ -645,31 +645,31 @@ function process_inputs(){
         return;
     }
     if(is_user_logged_in()){
-        require 'lib/functions.php';
-        global $price;
-        $final_price = str_replace($price, ',', '');
-        $current_user = get_current_user_data();
-        $phone_number = $current_user['phone_number'];
-        $parameters = array(
-            "merchant"=> ZIBAL_MERCHANT_KEY,//required
-            "callbackUrl"=> ZIBAL_CALLBACK_URL,//required
-            "amount"=> $final_price,//required
-
-            "orderId"=> time(),//optional
-            "mobile"=> "$phone_number",//optional for mpg
-        );
-
-        $response = postToZibal('request', $parameters);
-        var_dump($response);
-        if ($response->result == 100)
-        {
-            $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
-            header('location: '.$startGateWayUrl);
-        }
-        else{
-            echo "errorCode: ".$response->result."<br>";
-            echo "message: ".$response->message;
-        }
+//        require 'lib/functions.php';
+//        global $price;
+//        $final_price = str_replace($price, ',', '');
+//        $current_user = get_current_user_data();
+//        $phone_number = $current_user['phone_number'];
+//        $parameters = array(
+//            "merchant"=> ZIBAL_MERCHANT_KEY,//required
+//            "callbackUrl"=> ZIBAL_CALLBACK_URL,//required
+//            "amount"=> $final_price,//required
+//
+//            "orderId"=> time(),//optional
+//            "mobile"=> "$phone_number",//optional for mpg
+//        );
+//
+//        $response = postToZibal('request', $parameters);
+//        var_dump($response);
+//        if ($response->result == 100)
+//        {
+//            $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
+//            header('location: '.$startGateWayUrl);
+//        }
+//        else{
+//            echo "errorCode: ".$response->result."<br>";
+//            echo "message: ".$response->message;
+//        }
 
 
     }else{
