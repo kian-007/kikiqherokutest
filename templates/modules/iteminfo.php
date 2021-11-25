c7f9ce076f980680c9b3b94f5ce6e69c94b300e7
kian <king.kian007@gmail.com>
2021-11-25 14:38:51 +0330

    dargah

--- templates/modules/iteminfo.php
+++ templates/modules/iteminfo.php
@@ -646,34 +646,32 @@ function process_inputs(){
     }
     if(is_user_logged_in()){
         require_once ('lib/functions.php');
-//        global $price;
-//        $price = str_replace($price, ',', '');
-//        $price = (int)$price;
-//        $number = $_POST['number'];
-//        $number = (int)$number;
-//        $final_price = ($price*$number);
-//        $current_user = get_current_user_data();
-//        $phone_number = $current_user['phone_number'];
+        global $price;
+        $price = intval(str_replace($price, ',', ''));
+        $number = intval($_POST['number']);
+        $final_price = ($price*$number);
+        $current_user = get_current_user_data();
+        $phone_number = $current_user['phone_number'];
 
-//        $parameters = array(
-//            "merchant"=> ZIBAL_MERCHANT_KEY,//required
-//            "callbackUrl"=> ZIBAL_CALLBACK_URL,//required
-//            "amount"=> $final_price,//required
-//            "orderId"=> time(),//optional
-//            "mobile"=> $phone_number,//optional for mpg
-//        );
+        $parameters = array(
+            "merchant"=> ZIBAL_MERCHANT_KEY,//required
+            "callbackUrl"=> ZIBAL_CALLBACK_URL,//required
+            "amount"=> $final_price,//required
+            "orderId"=> time(),//optional
+            "mobile"=> $phone_number,//optional for mpg
+        );
 
-//        $response = postToZibal('request', $parameters);
-//        var_dump($response);
-//        if ($response->result == 100)
-//        {
-//            $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
-//            header('location: '.$startGateWayUrl);
-//        }
-//        else{
-//            echo "errorCode: ".$response->result."<br>";
-//            echo "message: ".$response->message;
-//        }
+        $response = postToZibal('request', $parameters);
+        var_dump($response);
+        if ($response->result == 100)
+        {
+            $startGateWayUrl = "https://gateway.zibal.ir/start/".$response->trackId;
+            header('location: '.$startGateWayUrl);
+        }
+        else{
+            echo "errorCode: ".$response->result."<br>";
+            echo "message: ".$response->message;
+        }
 
 
     }else{
