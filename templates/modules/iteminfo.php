eafcda3b5ae043d364a0485151ea2bd6346ae359
kian <king.kian007@gmail.com>
2021-11-27 10:27:05 +0330

    dargah

--- templates/modules/iteminfo.php
+++ templates/modules/iteminfo.php
@@ -1170,7 +1170,7 @@ function get_script(){ ?>
                     }
                 })
                 <?php $module = get_module_name(); ?>
-                $.post("<?php echo home_url($module); ?>",{num: num}, function(data, status, xhr) {
+                $.post("#",{num: num}, function(data, status, xhr) {
                     if (status == "success"){
                         var success = 1;
                     }
