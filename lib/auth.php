30f96b99388130a7da60330ac8e3c7c865c67117
kian <king.kian007@gmail.com>
2021-12-03 05:02:49 +0330

    expire

--- lib/auth.php
+++ lib/auth.php
@@ -4,7 +4,7 @@ $current_user = null;
 $current_user_id = null;
 
 //ABOUT two month
-define('SESSION_EXPIRATION', 1296000);
+define('SESSION_EXPIRATION', 1296001);
 
 function get_current_user_data(){
     global $current_user;
